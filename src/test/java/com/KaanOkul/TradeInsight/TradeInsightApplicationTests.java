ae4c031c2b2bbbf991f4ea2c64834904e1483edb
KaanIsmet <kaanismet7@gmail.com>
2024-10-11 23:28:40 +0300

    connected to the database

---
+++ src/test/java/com/KaanOkul/TradeInsight/TradeInsightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KaanOkul.TradeInsight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeInsightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

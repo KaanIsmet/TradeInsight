4652971c8100cce69c9d970a0efd1902b63c20eb
KaanIsmet <kaanismet7@gmail.com>
2024-11-09 15:56:14 +0300

    integrated alpha vantage api onto the controller and service

---
+++ src/main/java/com/KaanOkul/TradeInsight/service/ApiBuilder.java
@@ -0,0 +1,11 @@
+package com.KaanOkul.TradeInsight.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ApiBuilder {
+
+    public String build(String initialUrl,String symbol, String function, String interval, String apiKey) {
+        String url = String.format("%s%s&symbol=%s&interval=%s&apikey=%s", initialUrl, function, symbol, interval,apiKey);
+        return url;
+    }
+}
